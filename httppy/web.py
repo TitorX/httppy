c8f13f701f687f69b001e86c843a6b93e39fc677
titorx <sun.qd.china@outlook.com>
2015-09-17 16:20:38 +0800

    调整web代码 准备具体实现UrlRoute

--- httppy/web.py
+++ httppy/web.py
@@ -4,6 +4,29 @@ __author__ = 'titorx'
 import httppy.httpserver as httpserver
 
 
+class Request(httpserver.HttpRequest):
+    pass
+
+
+class Response(httpserver.HttpResponse):
+    pass
+
+
+class Response404(Response):
+    def __init__(self):
+        Response.__init__(self)
+        self.set_status(404)
+
+
+class WebHandler(httpserver.BaseHttpHandler):
+    def __init__(self, socket_request, client_address, server, url_route):
+        self.url_route = url_route
+        httpserver.BaseHttpHandler.__init__(self, socket_request, client_address, server)
+
+    def handle_http_request(self):
+        return self.url_route.route(self.http_request)
+
+
 class WebServer(httpserver.BaseHttpServer):
 
     class _Handler(httpserver.BaseHttpServer._Handler):
@@ -14,23 +37,19 @@ class WebServer(httpserver.BaseHttpServer):
         def handle_request(self):
             self.handler(self.socket_request, self.client_address, self.server, self.url_route)
 
-    def __init__(self, server_address, request_handler_class, url_route):
+    def __init__(self, server_address, url_route):
         self.url_route = url_route
-        httpserver.BaseHttpServer.__init__(self, server_address, request_handler_class)
+        httpserver.BaseHttpServer.__init__(self, server_address, WebHandler)
 
     def create_thread(self):
         return self._Handler(self, self.url_route)
 
 
-class Request(httpserver.HttpRequest):
-    pass
-
-
-class Response(httpserver.HttpResponse):
-    pass
-
-
 class WebRequestHandler:
+
+    """
+    处理UrlRoute分发的请求
+    """
 
     def __init__(self):
         pass
@@ -43,24 +62,13 @@ class UrlRoute:
         负责将对应url请求分发给对应handler
     """
 
-    def __init__(self, table):
-        self.table = table
+    def __init__(self, route_table):
+        self.route_table = route_table
 
     def route(self, request=Request()):
-        handler = self.table.get(request.url)
+        handler = self.route_table.get(request.url)
         if handler:
             response = handler(request)
         else:
-            response = Response()
-            response.set_status(404)
-            response.body = 'not found'
+            response = Response404()
         return response
-
-
-class WebHandler(httpserver.BaseHttpHandler):
-    def __init__(self, socket_request, client_address, server, url_route):
-        self.url_route = url_route
-        httpserver.BaseHttpHandler.__init__(self, socket_request, client_address, server)
-
-    def handle_http_request(self):
-        return self.url_route.route(self.http_request)
--- main.py
+++ main.py
@@ -1,24 +1,7 @@
 # coding=utf-8
-# from httppy import log
 from httppy import web
-import thread, time, SimpleHTTPServer, sys
 
 server_address = ('', 7777)
-
-
-class Handler(web.WebHandler):
-    pass
-    # def handle_http_request(self):
-        # import time
-        # time.sleep(10)
-        # self.http_response.body = 'hello world!'
-        # print(self.http_request.ip)
-        # print len(self.server.thread_pool)
-        # import json
-        # print('POST:' + json.dumps(self.http_request.POST, indent=4))
-        # print('GET:' + json.dumps(self.http_request.GET, indent=4))
-        # self.http_response.set_cookie('test1', '123', expires=10)
-        # self.http_response.set_cookie('test2', '123', expires=10)
 
 
 def p(request):
@@ -28,5 +11,5 @@ def p(request):
     return response
 
 url_route = web.UrlRoute({'/': p})
-app = web.WebServer(server_address, Handler, url_route)
+app = web.WebServer(server_address, url_route)
 app.server_start()
